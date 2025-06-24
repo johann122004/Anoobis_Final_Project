1f26d2549b13c17897a25fd3cd42e2d256b73f7a
johann122004 <johann122004@gmail.com>
2025-06-24 22:00:08 +0800

    feat: add description and words for buttons

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1>"Craving the taste of the afterlife? One sip, and you're resurrected."</h1>
+    <h1>"A brew of Ancient magic and modern mastery - every sip awakens the gods."</h1>
+    <p>Our Products</p>
+    <p>NOM NOM NOM</p>
 </body>
 </html>
