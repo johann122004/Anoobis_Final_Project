12908cd641279a694faee0e825e9ba1d704b7e3e
11JahazFEU <202310071@fit.edu.ph>
2025-06-25 07:42:05 +0800

    test

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title> Home </title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-
-    <div class="Top"> //This servers as the main header design that has logo and buttons for navigation
+    eloooooooo
+    <div class="Top"> #This servers as the main header design that has logo and buttons for navigation
         
     </div>
 
