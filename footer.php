741b896a1ba205ae57dda23749718a1781709047
11JahazFEU <202310071@fit.edu.ph>
2025-06-25 08:10:38 +0800

    test

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 <footer>
     <p>&copy; 2023 Your Company Name. All rights reserved.</p>
-    <?php include ('index.php'); ?>
+    <?php include ('header.php'); ?>
+
+
+
+    <p><a href="/Pages/Product List/Product.php"> Our Products</a></p>
+    <p> <a href="/Pages/Shopping/Shop.php">NOM NOM NOM</a></p>
+
+
+
 </footer>
--- header.php
+++ header.php
@@ -2,4 +2,5 @@
     <p> <a href="/Pages/Product List/Product.php"> Product </a></p>
     <p> <a href="/Pages/Shopping/Shop.php"> Shopping </a></p>
     <p> <a href="/Pages/Receipt/Receipt.php"> Receipt </a></p>
+    <?php include ('footer.php'); ?>
 </header>
