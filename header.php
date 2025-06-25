e956800a68e50b005d8f0bb0f9863612fcea3c70
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-25 23:59:39 +0800

    refactor: simplified code

--- header.php
+++ header.php
@@ -1,22 +1,5 @@
 <header>
-
-
-
-
-    <div class="HEADER">
     LOGO on the upper left side
-        <p> <a href="/Pages/Product List/Product.php"> Menu </a></p>
-        <p> <a href="/Pages/Shopping/Shop.php"> Shopping </a></p>
-
-
-    </div>
-    
-        THIS IS THE HEADER
-
-
-        <br><br>
-
-
-
-
+    <a href="/Pages/Product List/Product.php"> Menu </a>
+    <a href="/Pages/Shopping/Shop.php"> Shopping </a>
 </header>
