75008734b41221bfcf1635e30c1e0f45a9fd81a2
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-26 00:01:01 +0800

    refactor: simplified code

--- footer.php
+++ footer.php
@@ -1,19 +1,9 @@
 <footer>
-    
-
-
     <div class="FOOTER">
-        
         <p><a href="/Pages/Contact Us/ContactUs.php"> Contact Us </a></p>
     <br>
-        THIS IS THE FOOTER
         <p> ANOOBIS CAFE INC. </p>
         <p> Company registration no: 383-888-380-8888 </p>
         <p>&copy; 2025 Anoobis Cafe. All rights reserved. </p>
-
-
     </div>
-
-
-
 </footer>
