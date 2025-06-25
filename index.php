a970e3748ea9ad1d5fd84ced5717b6880a8cfeab
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-25 09:16:58 +0800

    feat: add require and <?php?> for Top and Bottom

--- index.php
+++ index.php
@@ -12,17 +12,25 @@
 
 welcome to the best coffee shop in the world!
     eloooooooo
-    <div class="Top"> #This servers as the main header design that has logo and buttons for navigation
-        
+    <div class="Top">
+    <?php #This servers as the main header design that has logo and buttons for navigation
+    require 'header.php';
+    ?>
+
     </div>
 
     ello testing
-    
+
     <h1>"Craving the taste of the afterlife? One sip, and you're resurrected."</h1>
     <h1>"A brew of Ancient magic and modern mastery - every sip awakens the gods."</h1>
     <p><a href="/Pages/Product List/Product.php"> Our Products</a></p>
     <p> <a href="/Pages/Shopping/Shop.php">NOM NOM NOM</a></p>
-    <div class="Bottom"></div> This section is the footer, containing info about Anoobis and contact info that links to another page
-    
+
+    <div class="Bottom">
+    <?php #This section is the footer, containing info about Anoobis and contact info that links to another page
+    require 'footer.php';
+    ?>
+    </div>
+
 </body>
 </html>
