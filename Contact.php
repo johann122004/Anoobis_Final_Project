4c8ecb6ec5298af4910b6d124cc495c824e48557
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-26 23:20:40 +0800

    feat: add profile picture, information of members, and contact information.

--- Contact.php
+++ Contact.php
@@ -11,8 +11,30 @@
         <?php #This serves as the main header design that has logo and buttons for navigation
             require ('header.php');
         ?>
+    <br><br><br><br><br><br><br>
     </div>
-    <br><br><br><br><br><br>
+    <div class = "Profile">
+        <img src="/Lopez_pfp.jpg" alt="">
+        <h1>Name: Johann Lopez</h1>
+        <h1>Contributions: Web Structure Designer. Web Code Programmer</h1>
+        <h1>Description: Johann A. Lopez is one of the main 3  developers of this website. Always gets the job done and always eager to help out! </h1>
+        <br><br>
+        <img src="/Castillo_pfp.jpg" alt="">
+        <h1>Name: Jahaziah Castillo</h1>
+        <h1>Contributions: Web Aesthetics Designer</h1>
+        <h1>Description: </h1>
+        <br><br>
+        <img src="/Eros_pfp.jpg" alt="">
+        <h1>Name: Reanne Eros Gabay</h1>
+        <h1>Contributions: Web Code Programmer</h1>
+        <h1>Description: </h1>
+        <br><br>
+    </div>
+    <div class = "Info">
+        <h1>Information to Contact the Company</h1>
+        <h1>Email: Anoobis@gmail.com---------Phone Number: 0934-567-1234---------Address: 5326 Elmore Street.</h1>
+    </div>
+    <br><br><br><br><br><br><br>
     <div class="Bottom">
         <?php #This section is the footer, containing info about Anoobis and contact info that links to another page
         require ('footer.php');
