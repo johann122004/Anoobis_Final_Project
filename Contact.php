232ea5a53733a5a722a5d67b45db62206f61b022
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-27 07:30:14 +0800

    fix: fix hyperlink to images

--- Contact.php
+++ Contact.php
@@ -14,17 +14,17 @@
     <br><br><br><br><br><br><br>
     </div>
     <div class = "Profile">
-        <img src="/Lopez_pfp.jpg" alt="">
+        <img src="Lopez_pfp.jpg" alt="">
         <h1>Name: Johann Lopez</h1>
         <h1>Contributions: Web Structure Designer. Web Code Programmer</h1>
         <h1>Description: Johann A. Lopez is one of the main 3  developers of this website. Always gets the job done and always eager to help out! </h1>
         <br><br>
-        <img src="/Castillo_pfp.jpg" alt="">
+        <img src="Castillo_pfp.jpg" alt="">
         <h1>Name: Jahaziah Castillo</h1>
         <h1>Contributions: Web Aesthetics Designer</h1>
         <h1>Description: Jahaziah is an Animation and Game Development student in FEU Tech. She creates simple, artistic, and eye catching web design for the group's project while sticking to time manageable aesthetics. She is willing to learn on how to code and develop logical skills in problem solving as well as to gain experience in group collaboration and communication. While having the love of passion in arts and carefully checking and fixing the group's output. </h1>
         <br><br>
-        <img src="/Eros_pfp.jpg" alt="">
+        <img src="Eros_pfp.jpg" alt="">
         <h1>Name: Reanne Eros Gabay</h1>
         <h1>Contributions: Web Code Programmer</h1>
         <h1>Description: Currently a student at FEU TECH, with a want to learn how to create an eye catching UI, hopes that this project will be just that. </h1>
