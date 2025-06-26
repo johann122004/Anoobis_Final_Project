b26dadfbd20de15ac2abaeaba111b8699d54a7a4
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-06-27 07:54:32 +0800

    feat: add images to left and right div

--- index.php
+++ index.php
@@ -16,11 +16,13 @@
     <div class = Content>
         <div class = "Left">
             <h1>"Craving the taste of the afterlife? One sip, and you're resurrected."</h1>
+            <img src="Coffee_Beans.png" alt="" width="55%" height="55%">
             <p> <a href="Shop.php">NOM NOM NOM</a></p>
         </div>
 
         <div class = "Right">
             <h1>"A brew of Ancient magic and modern mastery - every sip awakens the gods."</h1>
+            <img src="Coffee.png" alt="" width="55%" height="55%">
             <p><a href="Product.php"> Our Products</a></p>
         </div>
     </div>
