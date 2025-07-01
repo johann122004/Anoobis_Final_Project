ad3889b5d7a31e6e1829b005dafa47b811f48087
Johann Lopez <143859154+johann122004@users.noreply.github.com>
2025-07-01 13:13:06 +0800

    feat: add products, description and price.

--- Product.php
+++ Product.php
@@ -13,6 +13,46 @@
         ?>
     </div>
     <br><br><br><br><br><br>
+
+    <div class ="Coffee">
+        <div class = "product1">
+            <img src="Anubis_Dark_Roast.jpg" alt="" width="300" height="300">
+            <p>Name: Anubis' Dark Roast</p>
+            <p>Description: Rich, Bold, Anubis' Dark Roast will energize you with the energy of the God of the Afterlife.</p>
+            <p>Price: PHP 90.00</p>
+        </div>
+        <div class = "product2">
+            <img src="Jackal's_Delight.jpg" alt="" width="300" height="300">
+            <p>Name: Jackal's Delight</p>
+            <p>Description: Experience the life of a Jackal and scream in joy as you taste in it's special delightful drink.</p>
+            <p>Price: PHP 95.00</p>
+        </div>
+        <div class = "product3">
+            <img src="Coffin_Spice.jpg" alt="" width="300" height="300">
+            <p>Name: Coffin Spice</p>
+            <p>Description: Spice your life up in the tomb that resides the Coffin Spice.</p>
+            <p>Price: PHP 100.00</p>
+        </div>
+        <div class = "product4">
+            <img src="Pharaoh's_Nectar.jpg" alt="" width="300" height="300">
+            <p>Name: Pharaoh's Nectar</p>
+            <p>Description: Sweet and Elegant. Dare to taste Royalty as you indulge on the Pharaoh's Nectar sweets.</p>
+            <p>Price: PHP 130.00</p>
+        </div>
+        <div class = "product5">
+            <img src="Ankh_Americano.jpg" alt="" width="300" height="300">
+            <p>Name: Ankh Americano</p>
+            <p>Description: Unlock the Key of Life through the indulgence of the Ankh Americano.</p>
+            <p>Price: PHP 110.00</p>
+        </div>
+        <div class = "product6">
+            <img src="Golden_Scarab_Latte.jpg" alt="" width="300" height="300">
+            <p>Name: Golden Scarab Latte</p>
+            <p>Description: Witness a true Egyptian treasure through the Golden Scarab Latte, with it's signature golden texture and even more golden flavor.
+            <p>Price: PHP 125.00</p>
+        </div>
+    </div>
+    <br><br><br><br><br>
     <div class="Bottom">
         <?php #This section is the footer, containing info about Anoobis and contact info that links to another page
         require ('footer.php');
