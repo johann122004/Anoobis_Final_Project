061d03e4c552f1402448f008f75ebbe35ae5d11a
11JahazFEU <202310071@fit.edu.ph>
2025-07-02 07:34:15 +0800

    header and css minor change

--- header.php
+++ header.php
@@ -1,6 +1,13 @@
 <header>
-    <img src="Logo.png" alt="" width="5%" height="5%">
-    <a href="home.php"> Home </a>
-    <a href="Product.php"> Menu </a>
-    <a href="Shop.php"> Shopping </a>
+
+    <div class = "logo">
+        <img src="Logo.png" alt="" width="5%" height="5%">
+    </div>
+
+    <div class = "hButton">
+        <a href="home.php"> Home </a>  
+        <a href="Product.php"> Menu </a>
+        <a href="Shop.php"> Shopping </a>
+    </div>
+
 </header>
